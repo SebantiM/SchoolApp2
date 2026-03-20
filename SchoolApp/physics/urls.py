dcc2a7bf9eed105aa6362b7dd9bb327d3e0683ed
SebantiM <sebanti_mofokeng@yahoo.com>
2026-03-20 11:22:43 +0200

    finally using htmx

--- SchoolApp/physics/urls.py
+++ SchoolApp/physics/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'physics'
 urlpatterns = [
     path('grade12', views.g12home, name = 'g12home'),
+    path('grade12/reading', views.reading, name = 'reading')
 ]
--- SchoolApp/physics/views.py
+++ SchoolApp/physics/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 def g12home(request):
     context = {"list":[1,2,3,4,5]}
     return render(request, 'physics/grade12/home.html', context)
+
+def reading(request):
+    context = {"list":[1,2,3,4,5]}
+    return render(request, 'physics/grade12/fragments/reading.html', context)
